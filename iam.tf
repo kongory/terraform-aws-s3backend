f01f2574991fb7cfd233d6a0f266dc467b2bac0b
Kanstantsin Harbachou <k.harbachou@itechart-group.com>
2021-12-14 10:10:08 +0300

    Added tf configs

---
+++ iam.tf
@@ -0,0 +1,57 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  principal_arns = var.principal_arns != null ? var.principal_arns : [data.aws_caller_identity.current.arn]
+}
+
+resource "aws_iam_role" "iam_role" {
+  name               = "${local.namespace}-tf_assume_role"
+  assume_role_policy = <<-EOF
+  {
+      "Version":"2012-10-17"
+      "Stetement":[
+          {
+              "Action":"sts:AssumeRole",
+              "Principal":{"AWS":${jsonencode(local.principal_arns)}},
+              "Effect":"Allow"
+          }
+      ]
+  }
+  EOF
+
+  tags = {
+    ResourceGroup = local.namespace
+  }
+}
+
+data "aws_iam_policy_document" "policy_doc" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.s3_bucket.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItems",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [aws_dynamodb_table.dynamodb_table.arn]
+  }
+}
+
+resource "aws_iam_policy" "iam_policy" {
+  name   = "${local.namespace}-tf-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.policy_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.iam_policy.arn
+}
